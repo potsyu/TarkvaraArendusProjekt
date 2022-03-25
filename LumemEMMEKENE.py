c36a76cedbca4580ba14642a8929aabfbcc8621c
GamingWithKarl <101526013+GamingWithKarl@users.noreply.github.com>
2022-03-25 22:55:30 +0200

    Update LumemEMMEKENE.py

--- LumemEMMEKENE.py
+++ LumemEMMEKENE.py
@@ -5,7 +5,7 @@ import pygame #Sellel real imporditakse Pygame
 pygame.init() #See rida sisestab kõik pygame moodulid programmi.
 
 screen = pygame.display.set_mode([300,300]) #See rida teeb uue akna, kuhu ilmuvad programmis kirjutatud kujutised 300 * 300.
-pygame.display.set_caption("Lumemees - Karl Karulin :)") #See rida paneb aknale pealkirja
+pygame.display.set_caption("Lumemees - #INSERT NAME HERE#") #See rida paneb aknale pealkirja
 screen.fill([0, 0, 0])
 
 #Joonistamine

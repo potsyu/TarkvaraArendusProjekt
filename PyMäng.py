ac03c4e6263e6c318290a41a3f0915fb964fa119
bjorn.johanson <bjorn.johanson@vikk.ee>
2022-04-08 09:47:48 +0300

    TERE!

--- PyMäng.py
+++ PyMäng.py
@@ -5,28 +5,19 @@ screen=pygame.display.set_mode([640,480])
 pygame.display.set_caption("Ülesanne2")
 screen.fill([204, 255, 204])
 
-#lisame teksti
-font = pygame.font.SysFont("Comic Sans MS", 50)
-font.set_bold(True)
-font.set_underline(True)
-font.set_italic(True)
-text = font.render("Hello PyGame", True, [0,0,0])
 
 
-#tekstikasti suurus
-text_width = text.get_rect().width
-text_height = text.get_rect().height
-screen.blit(text, [150,150])
-
-#Ilus pilt :)
 #Lisame pildid
 bg = pygame.image.load("bg_shop.jpg")
 screen.blit(bg,[0,0])
 
+#lisame teksti
+font = pygame.font.SysFont("Arial", 50)
+text = font.render("Hello PyGame", True, [255,255,255])
+screen.blit(text, [200,200])
+
 
 pygame.display.flip()
-
-
 
 
 #Alumine osa ei lase Pygame ekraanil ise kinni minna.

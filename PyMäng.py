19ddf5ee2392318b9e41fbbe02f528d163c0b406
bjorn.johanson <bjorn.johanson@vikk.ee>
2022-04-08 09:30:31 +0300

    TERE!

--- PyMäng.py
+++ PyMäng.py
@@ -11,3 +11,13 @@ text = font.render("Hello PyGame", True, [0,0,0])
 screen.blit(text, [200,200])
 
 pygame.display.flip()
+
+
+#Alumine osa ei lase Pygame ekraanil ise kinni minna.
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+    if running == False:
+      pygame.quit()

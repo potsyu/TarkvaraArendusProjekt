bfa77ec549bf81fe35546300b7daff6dacc18586
bjorn.johanson <bjorn.johanson@vikk.ee>
2022-04-08 09:24:19 +0300

    TERE!

--- PyMäng.py
+++ PyMäng.py
@@ -1,6 +1,6 @@
 import pygame #Sellel real imporditakse Pygame
 pygame.init() #See rida sisestab kõik pygame moodulid programmi.
 
-screen = pygame.display.set_mode([300,300]) #See rida teeb uue akna, kuhu ilmuvad programmis kirjutatud kujutised 300 * 300.
+screen = pygame.display.set_mode([640,480]) #See rida teeb uue akna, kuhu ilmuvad programmis kirjutatud kujutised 300 * 300.
 pygame.display.set_caption("Lumemees - #INSERT NAME HERE#") #See rida paneb aknale pealkirja
 screen.fill([0, 0, 0])
